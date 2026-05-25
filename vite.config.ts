796d1fd116de4347202f036db78707d51c7d3f45
anastasia <anastasia.gribovskaia@gmail.com>
2026-05-25 12:09:58 +0300

    build: set up the basic project configuration

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+// import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import { fileURLToPath } from 'url';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    // tanstackRouter(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
