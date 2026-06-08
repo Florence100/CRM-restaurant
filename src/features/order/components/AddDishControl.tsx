1421ff8233ad07741b0628c260d46954005ceb20
anastasia <anastasia.gribovskaia@gmail.com>
2026-06-08 15:37:46 +0300

    feat: add styles to focused buttons

--- src/features/order/components/AddDishControl.tsx
+++ src/features/order/components/AddDishControl.tsx
@@ -9,7 +9,7 @@ export default function AddDishControl({ setIsMenuOpen }: AddDishControlProps) {
             <div className='flex items-center gap-4'>
                 <button
                     id='add-btn' 
-                    className='size-[26px] bg-black text-white rounded-sm font-bold cursor-pointer'
+                    className='size-[26px] bg-black text-white rounded-sm font-bold cursor-pointer focus:outline-offset-3'
                     onClick={() => setIsMenuOpen(true)}
                 >+</button>
                 <label htmlFor='add-btn' className='font-semibold opacity-40 cursor-pointer'>Add dish</label>
